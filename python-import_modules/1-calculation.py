cfd05cab7c2fd355d8a40d36525b2dd40ab8fa6b
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-19 03:02:52 -0500

    .

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+a=10
+b=5
+from calculator_1 import add
+print("{}".format(a,b))
+
+from calculator_1 import sub
+print("{}".format(a,b))
+
+from calculator_1 import mul
+print("{}".format(a,b))
+
+from calculator_1 import div
+print("{}".format(a,b))
