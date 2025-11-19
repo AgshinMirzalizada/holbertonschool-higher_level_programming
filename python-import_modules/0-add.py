0aad5b74f35476cbb4022757495c095f1129df99
AgshinMirzalizada <aqsin.mirzalizada@gmail.com>
2025-11-19 11:45:14 +0400

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 #a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
 
-from add_0.py from add
+from add_0 from add
 if __name__ == "__main__":
 a=1
 b=2
-print("{}+{}+{}".format(a,b,add(a,b)))
-
+print("{}+{}={}".format(a,b,add(a,b)))
