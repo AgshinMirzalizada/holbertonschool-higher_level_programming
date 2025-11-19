bbb9331bc310c7d03e194efbbb21fac40f487328
AgshinMirzalizada <aqsin.mirzalizada@gmail.com>
2025-11-19 12:03:36 +0400

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-a=10
-b=5
-from calculator_1 import add
-print("{}".format(a,b))
+from calculator_1 import add, sub, mul, div
 
-from calculator_1 import sub
-print("{}".format(a,b))
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-from calculator_1 import mul
-print("{}".format(a,b))
-
-from calculator_1 import div
-print("{}".format(a,b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
