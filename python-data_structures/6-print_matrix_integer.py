70dc34173a437e45a302e6330ccfb510da70581b
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-21 03:22:13 -0500

    .

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,14 @@ def print_matrix_integer(matrix=[[]]):
         for element in range(len(row)):
             if element != 0:
                 print(" ", end="")
-            print("{:d}".format(row[element], end=""))
+            print("{:d}".format(row[element]), end="")
         print()
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
