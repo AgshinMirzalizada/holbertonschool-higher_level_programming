095636856bc7d415cb850c153f27911aab4eb631
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-22 02:46:38 -0500

    .

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) > 0:
-        sort_list = sort(my_list)
+        sort_list = sorted(my_list)
         max_value = sort_list[-1]
+        return max_value
     else:
         return None
