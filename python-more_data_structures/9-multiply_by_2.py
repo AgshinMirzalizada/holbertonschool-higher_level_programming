0d6ce95171a4e12c645876827d5e9685e3fa59ed
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-23 11:33:20 -0500

    .

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,4 @@ def multiply_by_2(a_dictionary):
     for key in a_dictionary:
         new_value = a_dictionary[key] * 2
         new_dict[key] = new_value
-    return new_value
+    return new_dict
