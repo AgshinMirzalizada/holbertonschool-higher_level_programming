add4ef67372c66023001447fef37c80c57bb20c8
AgshinMirzalizada <aqsin.mirzalizada@gmail.com>
2025-11-24 11:16:18 +0400

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    couter = 0
+    counter = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
-            couter = couter + 1
+            counter = counter + 1
     except IndexError:
         pass
     print()
