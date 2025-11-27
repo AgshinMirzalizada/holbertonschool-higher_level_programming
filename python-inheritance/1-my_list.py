a0c185b0b286697136f056ccf20f288d954e2925
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-27 10:59:42 -0500

    .

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines MyList class, inherited from list"""
+
+class MyList(list):
+    """Class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """Prints the list, but sorted (ascending order)"""
+        print(sorted(self))
