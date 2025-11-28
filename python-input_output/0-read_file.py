eba0622e58754715ad21a06601341164286730de
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-28 03:21:43 -0500

    .

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 Module to read and print the content of a UTF-8 text file.
 """
 
+
 def read_file(filename=""):
     """Print the content of a UTF-8 text file."""
     with open(filename, "r", encoding="utf-8") as f:
