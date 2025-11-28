cc3b988c9faa71af39936b971defe5671f60a090
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-28 03:44:39 -0500

    .

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,8 @@
 Module to write a string to a UTF-8 text file.
 """
 
+
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 file and return the number of characters written."""
+    """Write a string to a UTF8 file and return numberof characters written."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
