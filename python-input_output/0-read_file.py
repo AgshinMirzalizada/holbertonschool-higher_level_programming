a28a28fd97ca654d3bdad483560d6760c90c0b81
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-28 03:20:13 -0500

    .

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """Print the content of a UTF-8 text file."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
