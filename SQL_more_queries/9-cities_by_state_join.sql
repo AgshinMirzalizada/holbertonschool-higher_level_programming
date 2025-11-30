7b6b88a44722d6c3aaef8a737cbd1db02ec64ed8
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-29 23:55:27 -0500

    .

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- ts is a comment
-SELECT cities.id, cities.name, stat.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
