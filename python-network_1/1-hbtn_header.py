abdc06b8c977f796f60dde49e5752e68198256db
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-12-02 13:05:47 -0500

    .

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get("X-Request-Id"))
