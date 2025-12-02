ac57e6cbc871afafd6a286c6d02a5f5974b6f439
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-12-02 09:59:58 -0500

    .

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+Fetches data from https://intranet.hbtn.io/status using the urllib package
+and displays the response body in a specific format.
+"""
 import urllib.request
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as request
-content = request.read()
-print("Body response:")
-print(f"\t- type: {type(content)}")
-print(f"\t- content: {content}")
-print(f"\t- utf8 content: {content.decode('utf-8')}")
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as request:
+    content = request.read()
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
